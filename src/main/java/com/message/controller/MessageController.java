6acfd5bba51b809c60902b319389e3d89961152f
SarathVenky <sarathvenky99@gmail.com>
2019-05-05 23:36:50 +0200

    Added read single message operation method

--- src/main/java/com/message/controller/MessageController.java
+++ src/main/java/com/message/controller/MessageController.java
@@ -1,11 +1,18 @@
 package com.message.controller;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
+
+import javax.validation.constraints.NotNull;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -34,7 +41,12 @@ public class MessageController {
 	
 	@Autowired
 	MessageRepository messageRepo;
-
+	
+	/**
+	 * 
+	 * @param messageRequest
+	 * @return ResponseEntity
+	 */
 	@PostMapping("/send")
 	@ApiOperation(
 	        value = "Send Message",
@@ -60,6 +72,12 @@ public class MessageController {
 			return new ResponseEntity<String>("Error in validating Request",HttpStatus.PRECONDITION_FAILED);
 		}
 	}
+	
+	/**
+	 * 
+	 * @param user
+	 * @return ResponseEntity<List<Message>>
+	 */
 	@ApiOperation(
 	        value = "Read Messages",
 	        notes = "This operation is to read messages sent by the user."
@@ -68,27 +86,68 @@ public class MessageController {
         @ApiResponse(code = 200, message = "Read Message"),
         @ApiResponse(code = 400, message = "Bad Request"),
         @ApiResponse(code = 404, message = "Not Found")})
-	@GetMapping("/receive/{user}")
-	public List<Message> readMessage(@PathVariable String user) {
+	@GetMapping("/read/sent/{user}")
+	public ResponseEntity<List<Message>> readMessagesSentByUser(@PathVariable @NotNull String user) {
 		Message message = new Message();
-		message.setReceiver(user);
-		Example<Message> example = Example.of(message);
-		return messageRepo.findAll(example);
-		
+		message.setSender(user);
+		return this.getMessagesResponse(message);
 	}
 	
+	/**
+	 * 
+	 * @param user
+	 * @return ResponseEntity<List<Message>>
+	 */
+	@ApiOperation(
+	        value = "Read Messages",
+	        notes = "This operation is to read messages received."
+	)
+	@ApiResponses({
+        @ApiResponse(code = 200, message = "Read Message"),
+        @ApiResponse(code = 400, message = "Bad Request"),
+        @ApiResponse(code = 404, message = "Not Found")})
+	@GetMapping("/read/received/{user}")
+	public ResponseEntity<List<Message>> readReceivedMessages(@PathVariable @NotNull String user) {
+		Message message = new Message();
+		message.setReceiver(user);
+		return this.getMessagesResponse(message);
+	}
+	
+	
+	/**
+	 * This method is used to call respository to get the list of messages and returns in the response entity
+	 * 
+	 * @param message
+	 * @return ResponseEntity
+	 */
+	private ResponseEntity<List<Message>> getMessagesResponse(Message message) {
+		Example<Message> example = Example.of(message);
+		List<Message> messages = messageRepo.findAll(example);
+		if(!CollectionUtils.isEmpty(messages)) {
+			return new ResponseEntity<List<Message>>(messages,HttpStatus.OK);	
+		}
+		else {
+			return new ResponseEntity<List<Message>>(Collections.emptyList(),HttpStatus.NOT_FOUND);
+		}
+	}
+	
+	/**
+	 * 
+	 * @param id
+	 * @return ResponseEntity<Message>
+	 */
 	@ApiOperation(
 	        value = "Read Single Message",
-	        notes = "This operation is to read a single message sent by the user which taking sender as input."
+	        notes = "This operation is to read a single message sent by the user which taking id of the message."
 	)
 	@ApiResponses({
         @ApiResponse(code = 200, message = "Read Messages"),
         @ApiResponse(code = 400, message = "Bad Request"),
         @ApiResponse(code = 404, message = "Not Found")})
-	@GetMapping("/readSent")
-	public List<MessageRequest> readSentMessages(@PathVariable String user) {
-		
-		return null;
+	@GetMapping("/read/{id}")
+	public ResponseEntity<Message> readMessage(@PathVariable Long id) {
+		Optional<Message> message = messageRepo.findById(id);
+		return new ResponseEntity<Message>(message.get(),HttpStatus.OK);
 		
 	}
 	

5889a4c9041a1dd2f2c7694184f05dc900f6ccff
SarathVenky <sarathvenky99@gmail.com>
2019-05-06 22:57:01 +0200

    Added methods to retrieve list of messages for sender and receiver

--- src/main/java/com/message/repo/MessageRepository.java
+++ src/main/java/com/message/repo/MessageRepository.java
@@ -1,5 +1,7 @@
 package com.message.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -15,5 +17,20 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Long>{
 	
-
+	/**
+	 * Method to retrieve list of messages sent by the sender
+	 * @param sender
+	 * @return List<Message>
+	 */
+	List<Message> findBySender(String sender);
+	
+	/**
+	 * Method to retrieve list of messages received by the user
+	 * @param receiver
+	 * @return List<Message>
+	 */
+	List<Message> findByReceiver(String receiver);
+	
 }
+
+
